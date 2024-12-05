78cbc03a81aa3b5768914bcf05f46972487832d4
tanmaykapoor <tanmay.skater@gmail.com>
2024-12-05 01:37:38 -0500

    add day 5 part 1

---
+++ src/day5/Solution.java
@@ -0,0 +1,66 @@
+package day5;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Solution {
+  int start() {
+    String dir = System.getProperty("user.dir") + "/src";
+    File file = new File(dir + "/day5/sample.txt");
+
+    int ans = 0;
+    try {
+      Scanner sc = new Scanner(file);
+      Set<String> set = new HashSet<>();
+
+      boolean isFirstHalf = true;
+      while (sc.hasNextLine()) {
+        String line = sc.nextLine();
+        if (line.equals("")) {
+          isFirstHalf = false;
+          continue;
+        }
+
+        if (isFirstHalf) {
+          set.add(line);
+        } else {
+          String[] nums = line.split(",");
+          boolean isValid = true;
+          for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+              String str = nums[j] + "|" + nums[i];
+              if (set.contains(str)) {
+                isValid = false;
+                break;
+              }
+            }
+            if (!isValid) {
+              break;
+            }
+          }
+
+          if (isValid) {
+            int val = Integer.parseInt(nums[nums.length / 2]);
+            ans += val;
+            System.out.println("valid " + val);
+          } else {
+            System.out.println("invalid");
+          }
+        }
+      }
+
+      return ans;
+    } catch (FileNotFoundException e) {
+      System.out.println("File not found");
+    }
+    return ans;
+  }
+
+  public static void main(String[] args) {
+    Solution ob = new Solution();
+    System.out.println(ob.start());
+  }
+}
