cac04755dc67d80a19a88ef885b3b8a52c12037a
tanmaykapoor <tanmay.skater@gmail.com>
2024-12-07 03:25:02 -0500

    add part 1

---
+++ src/day7/Solution.java
@@ -0,0 +1,64 @@
+package day7;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Solution {
+  boolean isPossible(String exp, long[] nums, long target, long curr, int i) {
+    if (curr > target) {
+      return false;
+    }
+
+    if (curr == target) {
+      if (i >= nums.length) {
+        System.out.println(exp + " = " + curr);
+        return true;
+      }
+    }
+
+    if (i >= nums.length) {
+      return false;
+    }
+
+    return isPossible(exp + "+" + nums[i], nums, target, curr + nums[i], i + 1) ||
+            isPossible(exp + "*" + nums[i], nums, target, curr * nums[i], i + 1);
+  }
+
+  long start() {
+    String dir = System.getProperty("user.dir") + "/src";
+    File file = new File(dir + "/day7/input.txt");
+
+    long ans = 0L;
+    try {
+      Scanner sc = new Scanner(file);
+
+      while (sc.hasNextLine()) {
+        String line = sc.nextLine();
+
+        String[] vals = line.split(": ");
+        long target = Long.parseLong(vals[0]);
+        String[] n = vals[1].split(" ");
+        long[] nums = new long[n.length];
+
+        for (int i = 0; i < n.length; i++) {
+          nums[i] = Long.parseLong(n[i]);
+        }
+
+        if (isPossible("", nums, target, 0, 0)) {
+          ans += target;
+        }
+      }
+
+      return ans;
+    } catch (FileNotFoundException e) {
+      System.out.println("File not found");
+    }
+    return ans;
+  }
+
+  public static void main(String[] args) {
+    Solution ob = new Solution();
+    System.out.println(ob.start());
+  }
+}
