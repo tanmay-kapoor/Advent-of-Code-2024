17c3279fe87ed160fef8a1c2d5074bf12ebf9c9e
tanmaykapoor <tanmay.skater@gmail.com>
2024-12-13 17:00:13 -0500

    use dfs instead of storing string

--- src/day11/Solution.java
+++ src/day11/Solution.java
@@ -5,38 +5,49 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Solution {
-  int start() {
+
+  long dfs(String val, int depth) {
+    if (depth == 25) {
+      return 1;
+    }
+
+    long cnt = 0L;
+
+    switch (val.length() % 2) {
+      case 0:
+        String p1 = val.substring(0, val.length() / 2);
+        String p2 = val.substring(val.length() / 2).replaceAll("^0+(?!$)", "");
+        cnt = dfs(p1, depth + 1) + dfs(p2, depth + 1);
+        break;
+
+      case 1:
+        if (val.equals("0")) {
+          cnt = dfs("1", depth + 1);
+        } else {
+          String nv = Long.toString(Long.parseLong(val) * 2024);
+          cnt = dfs(nv, depth + 1);
+        }
+        break;
+    }
+
+    return cnt;
+  }
+
+  long start() {
+    long ans = 0L;
     String dir = System.getProperty("user.dir") + "/src";
     File file = new File(dir + "/day11/input.txt");
 
-    int ans = 0;
     try {
       Scanner sc = new Scanner(file);
       String input = sc.nextLine();
-      StringBuilder str = new StringBuilder(input);
 
-      for (int i = 0; i < 25; i++) {
-//        System.out.println(i);
-        String[] arr = str.toString().split(" ");
-        str = new StringBuilder();
-        for (String val : arr) {
-          if (val.equals("0")) {
-            str.append("1");
-          } else if (val.length() % 2 == 1) {
-            str.append(Long.parseLong(val) * 2024);
-          } else {
-            String p1 = val.substring(0, val.length() / 2);
-            str.append(p1).append(" ");
+      String[] vals = input.split(" ");
 
-            String p2 = val.substring(val.length() / 2);
-            str.append(Long.parseLong(p2));
-          }
-
-          str.append(" ");
-        }
+      for (int i = 0; i < vals.length; i++) {
+        ans += dfs(vals[i], 0);
       }
 
-      return str.toString().trim().split(" ").length;
     } catch (FileNotFoundException e) {
       System.out.println("File not found");
     }
