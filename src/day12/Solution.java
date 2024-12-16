179c6b97652e7d5f8ff4f3fad6855c9204169ecf
tanmaykapoor <tanmay.skater@gmail.com>
2024-12-16 14:25:00 -0500

    add part 2

--- src/day12/Solution.java
+++ src/day12/Solution.java
@@ -10,6 +10,7 @@ import java.util.Set;
 
 public class Solution {
   int area = 0;
+  int corners = 0;
 
   void print(List<char[]> grid) {
     for (char[] ch : grid) {
@@ -42,6 +43,64 @@ public class Solution {
       return 1;
     }
 
+    char left = grid.get(i)[j - 1];
+    char right = grid.get(i)[j + 1];
+    char up = grid.get(i - 1)[j];
+    char down = grid.get(i + 1)[j];
+    char leftDiagUp = grid.get(i - 1)[j - 1];
+    char leftDiagDown = grid.get(i + 1)[j - 1];
+    char rightDiagUp = grid.get(i - 1)[j + 1];
+    char rightDiagDown = grid.get(i + 1)[j + 1];
+
+    boolean l = left != letter && left != '.';
+    boolean r = right != letter && right != '.';
+    boolean u = up != letter && up != '.';
+    boolean d = down != letter && down != '.';
+    boolean lu = leftDiagUp != letter && leftDiagUp != '.';
+    boolean ld = leftDiagDown != letter && leftDiagDown != '.';
+    boolean ru = rightDiagUp != letter && rightDiagUp != '.';
+    boolean rd = rightDiagDown != letter && rightDiagDown != '.';
+
+    // top left outward corner
+    if (l && u) {
+      corners++;
+    }
+
+    // top left inward corner
+    if (!l && !u && lu) {
+      corners++;
+    }
+
+    // top right outward corner
+    if (r && u) {
+      corners++;
+    }
+
+    // top right inward corner
+    if (!r && !u && ru) {
+      corners++;
+    }
+
+    // bottom left outward corner
+    if (l && d) {
+      corners++;
+    }
+
+    // bottom left inward corner
+    if (!l && !d && ld) {
+      corners++;
+    }
+
+    // bottom right outward corner
+    if (r && d) {
+      corners++;
+    }
+
+    // bottom right inward corner
+    if (!r && !d && rd) {
+      corners++;
+    }
+
     grid.get(i)[j] = '.';
     set.add(i + "," + j);
 
@@ -65,21 +124,24 @@ public class Solution {
       Set<String> set = new HashSet<>();
 
       while (sc.hasNextLine()) {
-        String line = sc.nextLine();
+        String line = "1" + sc.nextLine() + "1";
         grid.add(line.toCharArray());
       }
+      grid.addFirst("1".repeat(grid.getFirst().length).toCharArray());
+      grid.add("1".repeat(grid.getFirst().length).toCharArray());
 
       print(grid);
 
-      for (int i = 0; i < grid.size(); i++) {
-        for (int j = 0; j < grid.get(i).length; j++) {
+      for (int i = 1; i < grid.size() - 1; i++) {
+        for (int j = 1; j < grid.get(i).length - 1; j++) {
           if (!set.contains(i + "," + j)) {
             area = 0;
+            corners = 0;
             char letter = grid.get(i)[j];
             set.add(i + "," + j);
             int perimeter = dfs(copy(grid), i, j, letter, set);
-            System.out.println("letter: " + letter + " area: " + area + " perimeter: " + perimeter);
-            ans += (area * perimeter);
+            System.out.println("letter: " + letter + " area: " + area + " sides: " + corners);
+            ans += (area * corners);
 //            print(grid);
           }
         }
