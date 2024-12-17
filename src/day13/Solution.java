af88b13dfd655bf882a297fe9bf8d240f133a3d2
tanmaykapoor <tanmay.skater@gmail.com>
2024-12-17 13:30:43 -0500

    add part 2

--- src/day13/Solution.java
+++ src/day13/Solution.java
@@ -7,37 +7,37 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Solution {
-  int calcTokens(int[] x, int[] y, int px, int py) {
-    int c1 = x[0] * y[0];
+  long calcTokens(long[] x, long[] y, long px, long py) {
+    long c1 = x[0] * y[0];
 
-    int c2 = x[1] * y[0];
-    int v1 = y[0] * px;
+    long c2 = x[1] * y[0];
+    long v1 = y[0] * px;
 
-    int c3 = x[0] * y[1];
-    int v2 = x[0] * py;
+    long c3 = x[0] * y[1];
+    long v2 = x[0] * py;
 
     double val = (v1 - v2) / (c2 - c3 + 0.0);
-    if (val != (int) val) {
+    if (val != (long) val) {
       return 0;
     }
 
-    int b = (int) val;
+    long b = (long) val;
 
     val = (v1 - c2 * b) / (c1 + 0.0);
-    if (val != (int) val) {
+    if (val != (long) val) {
       return 0;
     }
 
-    int a = (int) val;
+    long a = (long) val;
 
     return a * 3 + b;
   }
 
-  int start() {
+  long start() {
     String dir = System.getProperty("user.dir") + "/src";
     File file = new File(dir + "/day13/input.txt");
 
-    int ans = 0;
+    long ans = 0;
     try {
       Scanner sc = new Scanner(file);
 
@@ -54,25 +54,23 @@ public class Solution {
         String[] vals = prize.split(", ");
         vals[0] = vals[0].substring(vals[0].indexOf("=") + 1);
         vals[1] = vals[1].substring(2);
-        int[] x = new int[2];
-        int[] y = new int[2];
-        int px = -1, py = -1, i = 0;
+        long[] x = new long[2];
+        long[] y = new long[2];
+        long px = -1, py = -1;
+        int i = 0;
 
         for (String line : lines) {
           Matcher matcher = pattern.matcher(line);
           if (matcher.find()) {
             // Extract components
-            String prefix = matcher.group(1); // Button A:, Button B:, Prize:
             String xValue = matcher.group(2); // X coordinate
             String yValue = matcher.group(3); // Y coordinate
 
-            // Store into a string array
-            String[] result = {prefix, xValue, yValue};
-            x[i] = Integer.parseInt(xValue);
-            y[i++] = Integer.parseInt(yValue);
+            x[i] = Long.parseLong(xValue);
+            y[i++] = Long.parseLong(yValue);
           } else {
-            px = Integer.parseInt(vals[0]);
-            py = Integer.parseInt(vals[1]);
+            px = Long.parseLong(vals[0]) + 10000000000000L;
+            py = Long.parseLong(vals[1]) + 10000000000000L;
           }
         }
 
